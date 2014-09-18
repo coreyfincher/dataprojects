8ab53e37078d2e39fc705fde8a478a67d47a5a3c
coreyfincher <coreylanefincher@gmail.com>
2014-09-18 18:36:14 +0100

    failed script

--- testCode.R
+++ testCode.R
@@ -1,1 +1,6 @@
 #this is a test script
+datavar <- read.csv("AGD_CP_trimmedbutnoexclusions.csv")
+attach(datavar)
+model<-lm(tdds_pathogen~sex+age+button_disease+button_architecture+button_guns+
+            (sex*button_disease)+(sex*button_architecture)+(sex*button_guns), datavar)
+summary(model)
